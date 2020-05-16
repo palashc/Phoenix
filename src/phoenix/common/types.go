747bbea7884512ced63c5b7e9d7e6aee4e8e5ed3
Vansh Singh <vanshchaudhary@gmail.com>
2020-05-16 11:18:01 -0700

    Stubs + setup

---
+++ src/phoenix/common/types.go
@@ -0,0 +1,16 @@
+package utils
+
+import (
+	"math/rand"
+)
+
+// Task simulates Phoenix tasks with a sleep time between 0 and 1 seconds
+type Task float32
+
+// Job is an array of Tasks
+type Job []Task
+
+// CreateRandTask creates Random-Lengthed Task that lasts between 0 and 1 seconds
+func CreateRandTask() Task {
+	return Task(rand.Float32())
+}
