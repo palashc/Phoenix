cc0bd73abb4eee0add8bfe59736ec8e0541b2fe3
Palash <palashc983@gmail.com>
2020-05-18 13:44:30 -0700

    rpc server

--- src/phoenix/monitor/server.go
+++ src/phoenix/monitor/server.go
@@ -1,1 +1,32 @@
 package monitor
+
+import (
+	"net"
+	"net/http"
+	"net/rpc"
+)
+
+func GetNewClient(addr string) *NodeMonitorClient {
+	return &NodeMonitorClient{addr: addr}
+}
+
+// Serve as a backend based on the given configuration
+func ServeMonitor(b *config.MonitorConfig) error {
+
+	server := rpc.NewServer()
+	server.Register(b.Monitor)
+
+	listener, err := net.Listen("tcp", b.Addr)
+	if err != nil {
+		if b.Ready != nil { //nil channel blocks
+			b.Ready <- false
+		}
+		return err
+	}
+
+	if b.Ready != nil { //nil channel blocks
+		b.Ready <- true
+	}
+
+	return http.Serve(listener, server)
+}
