26972a204b7ac450e77391c574f5ef2602f4678b
Palash <palashc983@gmail.com>
2020-05-18 16:31:01 -0700

    removed test method from types

--- src/phoenix/types/types.go
+++ src/phoenix/types/types.go
@@ -1,9 +1,5 @@
 package types
 
-import (
-	"math/rand"
-)
-
 // Task simulates Phoenix tasks with a sleep time between 0 and 1 seconds
 type Task struct {
 	id string
@@ -14,12 +10,6 @@ type Task struct {
 type Job struct {
 	id    string
 	tasks []Task
-}
-
-// CreateRandTask creates Random-Lengthed Task that lasts between 0 and 1 seconds
-// TODO random string for id
-func CreateRandTask() Task {
-	return Task{T: rand.Float32()}
 }
 
 // Task Reservation for late-binding
