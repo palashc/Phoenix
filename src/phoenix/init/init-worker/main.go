d24cd7a0561b2f3e92afb70649ea305e91599e26
Palash <palashc983@gmail.com>
2020-05-24 16:24:22 -0700

    separate init

--- src/phoenix/init/init-worker/main.go
+++ src/phoenix/init/init-worker/main.go
@@ -4,11 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"phoenix"
 	"phoenix/config"
 	"phoenix/executor"
 	"phoenix/monitor"
-	"phoenix/scheduler"
 )
 
 const DefaultSlotCount = 4
@@ -27,32 +25,20 @@ func main() {
 	rc, e := config.LoadConfig(*frc)
 	noError(e)
 
-	schedulerClientMap := make(map[string]phoenix.TaskSchedulerInterface)
-	for _, schedulerAddr := range rc.Schedulers {
-		schedulerClientMap[schedulerAddr] = scheduler.GetNewTaskSchedulerClient(schedulerAddr)
-	}
-
 	run := func(i int) {
 		if i > len(rc.Monitors) {
 			noError(fmt.Errorf("index out of range: %d", i))
 		}
 
-		executorAddr := rc.Executors[i]
 		nodeMonitorAddr := rc.Monitors[i]
 
-		executorClient := executor.GetNewClient(executorAddr)
 		monitorClient := monitor.GetNewClient(nodeMonitorAddr)
-
-		// TODO: get a better newXXXConfig method
-		mConfig := rc.NewMonitorConfig(i, monitor.NewNodeMonitor(executorClient, schedulerClientMap))
 
 		// default slot count = 4
 		eConfig := rc.NewExecutorConfig(i, executor.NewExecutor(i, DefaultSlotCount, monitorClient))
 
-		log.Printf("monitor serving on %s", mConfig.Addr)
 		log.Printf("executor serving on %s", eConfig.Addr)
 
-		go noError(monitor.ServeMonitor(mConfig))
 		noError(executor.ServeExecutor(eConfig))
 	}
 
